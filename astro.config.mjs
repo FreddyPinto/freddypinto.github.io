ca7e9cb4eba0a071dd296908627df3ef17b2cec1
FreddyPinto <freddypinto@outlook.com>
2025-04-07 14:24:48 -0500

    Add robots.txt integration to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://freddypinto.github.io',
-  integrations: [tailwind()]
+  site: "https://freddypinto.github.io",
+  integrations: [tailwind(), robotsTxt()],
 });
